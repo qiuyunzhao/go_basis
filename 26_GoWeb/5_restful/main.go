05061f0308bffa3bc7c4247abdc113dd1be7be31
qiuyunzhao <1102401880@qq.com>
2019-10-26 19:56:09 +0800

    增加restful

---
+++ 26_GoWeb/5_restful/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+//restful只是一种url风格
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/qyz/{url}", func1).Methods("GET") //http://127.0.0.1:8081/qyz/abc Get方式
+	http.ListenAndServe(":8081", r)
+}
+
+func func1(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintln(w, vars["url"])
+}

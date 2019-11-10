099fc3d54d92d66f8dba317699b49fb7f063194c
qiuyunzhao <1102401880@qq.com>
2019-11-10 12:01:00 +0800

    gin框架知识

---
+++ 28_Gin框架/2_GinHttpRouter/2_请求函数(常用)/RouterMain.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	engine := gin.Default() //Default 使用 Logger 和 Recovery 中间件 ; engine:=gin.New()不使用默认中间件
+
+	//------------------------------------- 2.该方式比 Handle 方法更常用-------------------------------------------------
+	//GET请求 http://localhost:8888/login?name=qyz
+	engine.GET("/login", login)
+
+	//POST请求 http://localhost:8888/login1   请求体 username  password
+	engine.POST("/login1", login1)
+
+	//DELETE请求 http://localhost:8888/user/123456
+	engine.DELETE("/user/:id", deleteUser)
+
+	//范绑定请求，匹配以 /login 开头的GET路由  http://localhost:8888/login/ji?name=qyz
+	engine.GET("/login/*action", startWith)
+
+	//Any请求，匹配任何请求方式 http://localhost:8888/any
+	engine.Any("/any", anyType)
+
+	engine.Run(":8888") // 监听并在 0.0.0.0:8888 上启动服务
+}
+
+//GET请求
+func login(context *gin.Context) { //context上下文环境 请求参数在其中
+	//获取解析接口
+	path := context.FullPath()
+	//获取请求参数
+	requestParam := context.Query("name") //获取不到时不带默认值
+	//requestParam := context.DefaultQuery("name", "获取不到时候的默认值") //获取不到时带默认值
+	//给请求端返回数据
+	context.Writer.Write([]byte("解析接口:" + path + "\t 获取到请求参数为：" + requestParam))
+}
+
+//POST请求
+func login1(context *gin.Context) {
+	//获取解析接口
+	path := context.FullPath()
+	//获取请求参数
+	userName := context.PostForm("username")
+	password, exist := context.GetPostForm("password") //返回password是否存在的标志
+	if !exist {
+		log.Println("获取不到请求参数-password")
+	}
+	//给请求端返回数据
+	context.JSON(200, gin.H{
+		"matchUrl": path,
+		"username": userName,
+		"password": password,
+	})
+}
+
+//DELETE请求
+func deleteUser(context *gin.Context) {
+	//获取解析接口
+	path := context.FullPath()
+	//获取变量参数 id 的值
+	userID := context.Param("id")
+	//给请求端返回数据
+	context.Writer.Write([]byte("解析接口:" + path + "\t 获取变量参数id的值：" + userID))
+}
+
+//匹配以 /login 开头的GET路由
+func startWith(context *gin.Context) {
+	//获取解析接口
+	path := context.FullPath()
+	//获取请求参数
+	requestParam := context.DefaultQuery("name", "默认值") //获取不到时带默认值
+	//给请求端返回数据
+	context.Writer.Write([]byte("解析接口:" + path + "\t 获取到请求参数为：" + requestParam))
+}
+
+//匹配任何请求方式
+func anyType(context *gin.Context) {
+	//获取解析接口
+	path := context.FullPath()
+	//获取请求参数
+	requestParam := context.DefaultQuery("name", "默认值") //获取不到时带默认值
+	//给请求端返回数据
+	context.Writer.Write([]byte("解析接口:" + path + "\t 获取到请求参数为：" + requestParam))
+}
---
+++ 28_Gin框架/4_多种请求返回格式/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+
+	egine := gin.Default()
+
+	//请求静态资源 第1步
+	egine.LoadHTMLGlob("28_Gin框架/html/*") //设置可加载HTML目录，要返回HTML类型需要先设置
+	//请求静态资源 第2步
+	egine.Static("/static", "28_Gin框架/static") //加载图片等静态资源要设置加载路径 relativePath:请求路由 root:项目静态资源路径
+	//egine.StaticFS("/static",http.Dir("28_Gin框架/static")) //同上
+	//egine.StaticFile("/static","28_Gin框架/static/赵敏.jpg") //请求单个静态文件
+
+	//GET请求： http://localhost:8888/returnDifferentType
+	egine.GET("returnDifferentType", returnDifferentType)
+
+	egine.Run(":8888")
+
+}
+
+//请求返回 字节数组 和 字符串
+func returnDifferentType(context *gin.Context) {
+	fullPath := "请求路径：" + context.FullPath()
+
+	////--------------------------------------------  1 返回字节数组 --------------------------------------------------
+	//context.Writer.Write([]byte("返回字节数组\n"))
+	//
+	////--------------------------------------------  2 返回字符串 ----------------------------------------------------
+	//context.Writer.WriteString("返回字符串\n")                 //方式1
+	//context.String(http.StatusOK,"%s,%s","字符串1","字符串2")  //方式2
+	//
+	////--------------------------------------------  3 返回 状态码+JSON ----------------------------------------------
+	////3.1 Map类型JSON
+	//context.JSON(200, map[string]interface{}{
+	//	//常用自定义格式：
+	//	"code":    1,        // code请求状态码 1:请求成功 0:请求失败
+	//	"message": "OK",     // message请求消息 成功：OK  失败:具体错误信息
+	//	"data":    fullPath, // data 具体返回数据
+	//})
+	//
+	////3.2 Struct类型JSON
+	//resp := Respone{
+	//	Code:    1,
+	//	Message: "OK",
+	//	Data:    fullPath,
+	//}
+	//context.JSON(200, &resp)
+
+	//--------------------------------------------  4 返回 HTML ---------------------------------------------------
+	//把前边几种返回方式注释掉,在浏览器中发出请求即可实现解析后的html展示
+
+	//请求静态资源 第3步
+	context.HTML(http.StatusOK, "index.html", gin.H{
+		"title":    "Gin返回Html格式数据",
+		"fullPath": fullPath,
+	})
+
+}
+
+//响应结构体
+type Respone struct {
+	Code    int         `json:"resp_code"`
+	Message string      `json:"resp_name"`
+	Data    interface{} `json:"resp_data"`
+}

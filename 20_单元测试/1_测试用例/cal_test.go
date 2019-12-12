1b4fd4c57670a25fa496830c44f2fddbefafa8d2
qiuyunzhao <1102401880@qq.com>
2019-12-12 21:57:31 +0800

    单元测试补充

--- 20_单元测试/1_测试用例/cal_test.go
+++ 20_单元测试/1_测试用例/cal_test.go
@@ -1,19 +1,21 @@
 package __测试用例
 
 import (
+	"fmt"
 	"testing" //引入go 的testing框架包
 )
+
+func TestMain(m *testing.M) {
+	fmt.Println("TestMain函数可以在测试函数执行之前做一些其他操作")
+	//TestMain（）函数中必须要加 m.Run() 方法，否则测试文件下的测试函数不会被执行
+	m.Run()
+}
 
 //编写要给测试用例，去测试addUpper是否正确
 func TestAddUpper(t *testing.T) {
 
 	//调用
 	res := addUpper(10)
-	if res != 55 {
-		t.Fatalf("AddUpper(10) 执行错误，期望值=%v 实际值=%v\n", 55, res)
-	}
-
-	//如果正确，输出日志
-	t.Logf("AddUpper(10) 执行正确...")
+	t.Log("AddUpper(10) 执行结果：", res)
 
 }

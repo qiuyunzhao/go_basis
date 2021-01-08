349dbd8ab4d8b6019119acd00fd39aaf96dccb3a
qiuyunzhao <1102401880@qq.com>
2021-01-08 18:32:33 +0800

    目录结构更新

---
+++ 99_工具类/07_读取电脑信息/03_ip/ip.go
@@ -0,0 +1,70 @@
+/*
+@ Time : 2021/1/8 15:35
+@ Author : qyz
+@ File : ipUtils
+@ Software: GoLand
+@ Description:
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+func main() {
+	ip, err := GetExternalIP()
+	if err != nil {
+		println(err)
+	}
+	fmt.Println(ip.String())
+}
+
+// 获取本机Ip
+func GetExternalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, errors.New("检查是否连接网络")
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
+	}
+
+	return ip
+}
